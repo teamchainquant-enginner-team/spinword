e12629d9ef5ddd42079d95e922ac9e09c6fec681
teamchainquant-enginner-team <teamchainquant@gmail.com>
2026-07-14 21:26:52 -0400

    Add files via upload

--- spinword-app/next.config.mjs
+++ spinword-app/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: ".next16",
   allowedDevOrigins: ["127.0.0.1"],
   serverExternalPackages: ["better-sqlite3"],
   poweredByHeader: false,
